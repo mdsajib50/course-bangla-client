608f3cbb485a7122061ede6d770d72cf7a0077d6
mdsajib50 <mdajib850@gmail.com>
2022-10-26 09:08:15 +0600

    title added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {NavLink} from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <div>
@@ -9,16 +11,33 @@ const Navbar = () => {
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
-                <li><NavLink>Item 1</NavLink></li>
+                <li><NavLink to=''>Item 1</NavLink></li>
                 <li tabIndex={0}>
-                    <a>
+                    <NavLink to=''>
                     Parent
                    
-                    </a>
+                    </NavLink>
                   
                 </li>
-                <li><a>Item 3</a></li>
+                <li><NavLink to=''>Item 3</NavLink></li>
                 </ul>
+                <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
             </div>
             </div>
         </div>
