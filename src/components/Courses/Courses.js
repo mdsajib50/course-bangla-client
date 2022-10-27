43e0dea0439828970795402f7e26ab403994cef8
mdsajib50 <mdajib850@gmail.com>
2022-10-27 18:24:51 +0600

    courses components added

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+const Courses = () => {
+    return (
+        <div className='flex'>
+           <div className='grid grid-cols-3 gap-2'>
+           <div className="card card-compact w-96 bg-base-100 shadow-2xl shadow-indigo-500/40">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+                </div>
+            </div>
+           </div>
+            <div>
+                <p>Category</p>
+            </div>
+    </div>
+    );
+};
+
+export default Courses;
