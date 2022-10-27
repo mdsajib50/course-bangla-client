edc2ebe0bce1b3d5bd53c7fbf0cdcc40ce783d81
mdsajib50 <mdajib850@gmail.com>
2022-10-27 18:14:44 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Main from '../src/components/Main/Main'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from '../src/components/Home/Home'
+import Courses from '../src/components/Courses/Courses'
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/',
 
-      element:<Main></Main>
+      element:<Main></Main>,
+
+      children: [
+        {
+          path: '/home',
+
+          element: <Home></Home>
+      },
+      {
+        path:'/courses',
+
+        element:<Courses></Courses>
+      }
+      ]
+      
     }
   ])
   return (
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,40 +5,38 @@ import {NavLink} from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-slate-300">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Courses-Bangla</a>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal p-0">
-                <li><NavLink to=''>Item 1</NavLink></li>
+                <ul className="menu menu-compact p-2 me-2">
+                <li><NavLink to='/home'>Home</NavLink></li>
                 <li tabIndex={0}>
-                    <NavLink to=''>
-                    Parent
-                   
-                    </NavLink>
+                    <NavLink to='/courses'>Courses</NavLink>
                   
                 </li>
-                <li><NavLink to=''>Item 3</NavLink></li>
+                <li><NavLink to='/blog'>Blog</NavLink></li>
+                <li><NavLink to='/faq'>FAQ</NavLink></li>
                 </ul>
                 <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src="https://placeimg.com/80/80/people" />
-        </div>
-      </label>
-      <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-            </div>
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                        </div>
+                 </div>
             </div>
         </div>
     );
